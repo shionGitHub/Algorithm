d1557f33761a48ed2c9bdabd0c29c718494ec64a
shion <346419807@qq.com>
2019-07-14 17:42:24 +0800

    添加Dijkstra加速的算法

--- Algorithm/src/com/yishion/algorithm/A/d6/Code06_Dijkstra.java
+++ Algorithm/src/com/yishion/algorithm/A/d6/Code06_Dijkstra.java
@@ -2,6 +2,7 @@ package com.yishion.algorithm.A.d6;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
@@ -89,6 +90,144 @@ public class Code06_Dijkstra {
 		for (Entry<Node, Integer> entry : map.entrySet()) {
 			System.out.println("节点： " + entry.getKey().value + "--------距离： "
 					+ entry.getValue());
+		}
+
+		NodeHeap nodeHeap = new NodeHeap(graph.nodes.size());
+
+		Node head = graph.nodes.get(1);
+
+		nodeHeap.addOrupdateOrIgnore(head, 0);
+
+		Map<Node, Integer> result = new HashMap<>();
+
+		while (!nodeHeap.isEmpty()) {
+			RecordItem item = nodeHeap.pop();
+			Node node = item.node;
+			int distance = item.distance;
+			result.put(node, distance);
+			for (int i = 0; i < node.edges.size(); i++) {
+				Edge edge = node.edges.get(i);
+				nodeHeap.addOrupdateOrIgnore(edge.toNode, edge.weight
+						+ item.distance);
+			}
+		}
+
+		for (Entry<Node, Integer> entry : result.entrySet()) {
+			System.out.println("节点： " + entry.getKey().value + "--------距离： "
+					+ entry.getValue());
+		}
+
+	}
+
+	public static class RecordItem {
+		public Node node;
+		public int distance;
+
+	}
+
+	public static class NodeHeap {
+		private Node[] nodes;
+		public Map<Node, Integer> nodeIndexMap;
+		public Map<Node, Integer> distanceMap;
+		public int size;
+
+		public NodeHeap(int size) {
+			nodes = new Node[size];
+			nodeIndexMap = new HashMap<>();
+			distanceMap = new HashMap<>();
+			this.size = 0;
+		}
+
+		public boolean isEmpty() {
+			return this.size == 0;
+		}
+
+		public void addOrupdateOrIgnore(Node node, int distance) {
+
+			if (!isEnter(node)) {
+				nodeIndexMap.put(node, size);
+				distanceMap.put(node, distance);
+				nodes[size++] = node;
+				heapInsert(size - 1);
+				return;
+			}
+
+			if (inHeap(node)) {
+				int dis = distanceMap.get(node);
+				if (dis > distance) {
+					distanceMap.put(node, distance);
+					int index = nodeIndexMap.get(node);
+					heapify(index, size);
+				}
+
+			}
+
+		}
+
+		public RecordItem pop() {
+			RecordItem item = new RecordItem();
+			item.node = nodes[0];
+			item.distance = distanceMap.get(nodes[0]);
+			distanceMap.remove(nodes[0]);
+			swap(0, size - 1);
+			nodeIndexMap.put(nodes[size - 1], -1);
+			nodes[size - 1] = null;
+			heapify(0, --size);
+			return item;
+		}
+
+		private boolean isEnter(Node node) {
+			return nodeIndexMap.containsKey(node);
+		}
+
+		public boolean inHeap(Node node) {
+			return isEnter(node) && nodeIndexMap.get(node) != -1;
+		}
+
+		public void heapInsert(int index) {
+			while (distanceMap.get(nodes[index]) < distanceMap
+					.get(nodes[(index - 1) / 2])) {
+				swap(index, (index - 1) / 2);
+				index = (index - 1) / 2;
+			}
+
+		}
+
+		public void heapify(int index, int size) {
+			int left = 2 * index + 1;
+
+			while (left < size) {
+				int smallest;
+				int right = left + 1;
+
+				if (right >= size) {
+					smallest = left;
+				} else {
+					if (distanceMap.get(nodes[left]) < distanceMap
+							.get(nodes[right])) {
+						smallest = left;
+					} else {
+						smallest = right;
+					}
+				}
+
+				if (distanceMap.get(nodes[index]) < distanceMap
+						.get(nodes[smallest])) {
+					break;
+				}
+				swap(index, smallest);
+				index = smallest;
+				left = 2 * index + 1;
+			}
+
+		}
+
+		private void swap(int i, int j) {
+			nodeIndexMap.put(nodes[i], j);
+			nodeIndexMap.put(nodes[j], i);
+			Node tempNode = nodes[i];
+			nodes[i] = nodes[j];
+			nodes[j] = tempNode;
 		}
 
 	}
